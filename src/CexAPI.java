907ec2474dc842e75a0b9dd5b8c22c1c03753b55
Zack Urben <zackurben@gmail.com>
2013-12-30 23:00:47 -0600

    Added methods comments

--- src/CexAPI.java
+++ src/CexAPI.java
@@ -3,7 +3,7 @@
  * you can read more in LICENSE.txt.
  *
  * CexAPI.java
- * Version		:	1.0.4
+ * Version		:	1.0.5
  * Author		:	Zack Urben
  * Contact		:	zackurben@gmail.com
  * Creation		:	12/29/13
@@ -40,6 +40,13 @@ public class CexAPI {
 	private final String apiSecret;
 	private int nonce;
 	
+	/**
+	 * Creates a CexAPI Object.
+	 * 
+	 * @param user (String) = Cex.io Username
+	 * @param key (String) = Cex.io API Key
+	 * @param secret (String) = Cex.io API Secret
+	 */
 	public CexAPI(String user, String key, String secret) {
 		this.username = user;
 		this.apiKey = key;
@@ -47,6 +54,12 @@ public class CexAPI {
 		this.nonce = (int) System.currentTimeMillis();
 	}
 	
+	/**
+	 * Debug the contents of the a CexAPI Object.
+	 * 
+	 * @return (String) = The CexAPI object data: username, apiKey,
+	 * apiSecret, and nonce.
+	 */
 	public String toString() {
 		return "{\"username\":\"" + this.username + "\",\"apiKey\":\"" + this.apiKey + "\",\"apiSecret:\"" +
 			this.apiSecret + "\",\"nonce:\"" + this.nonce + "\"}";
@@ -54,7 +67,8 @@ public class CexAPI {
 	
 	/**
 	 * Create HMAC-SHA256 signature for our POST call.
-	 * @return HMAC-SHA256 message for POST authentication.
+	 * 
+	 * @return (String) = HMAC-SHA256 message for POST authentication.
 	 */
 	private String signature() {
 		String message = new String(this.nonce + this.username + this.apiKey);
@@ -73,6 +87,14 @@ public class CexAPI {
 		return String.format("%X", new BigInteger(1, hmac.doFinal(message.getBytes())));
 	}
 	
+	/**
+	 * Make a POST request to the Cex.io API, with the given data.
+	 * 
+	 * @param addr (String) = HTTP Address to make the request.
+	 * @param param (String) = Parameters to add to the POST request.
+	 * @param auth (Boolean) = Authentication required flag.
+	 * @return (String) = Result from POST sent to server.
+	 */
 	private String post(String addr, String param, boolean auth) {		
 		URLConnection connection = null;
 		DataOutputStream output = null;
@@ -140,6 +162,15 @@ public class CexAPI {
 		return response;
 	}
 	
+	/**
+	 * Wrapper for post method; builds the correct URL for the POST request.
+	 * 
+	 * @param method (String) = Method for the POST request.
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @param param (String) = Parameters to add to the POST request.
+	 * @param auth (Boolean) = Authentication required flag.
+	 * @return (String) = Result from POST sent to server.
+	 */
 	private String apiCall(String method, String couple, String param, boolean auth) {
 		method = method + "/";
 		
@@ -151,30 +182,79 @@ public class CexAPI {
 		return this.post(("https://www.cex.io/api/" + method + couple), param, auth);
 	}
 	
+	/**
+	 * Fetch the ticker data, for the given currency pair. 
+	 * 
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @return (String) = The public ticker data for the given pair.
+	 */
 	public String ticker(String couple) {
 		return this.apiCall("ticker", couple, "", false);
 	}
 	
+	/**
+	 * Fetch the order book data, for the given currency pair. 
+	 * 
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @return (String) = The public order book data for the given pair.
+	 */
 	public String order_book(String couple) {
 		return this.apiCall("order_book", couple, "", false);
 	}
 	
+	/**
+	 * Fetch the trade history data, for the given currency pair. 
+	 * 
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @param since (Int) = Unix time stamp to retrieve the data from.  
+	 * @return (String) = The public trade history for the given pair
+	 * (Currently limited to the last 1000 trades).
+	 */
 	public String trade_history(String couple, int since) {
 		return this.apiCall("trade_history", couple, ("since," + since), false);
 	}
 	
+	/**
+	 * Fetch the account balance data, for the Cex.io API Object 
+	 *   
+	 * @return (String) = The account balance for all currency pairs.
+	 */
 	public String balance() {
 		return this.apiCall("balance", "", "", true);
 	}
 	
+	/**
+	 * Fetch the accounts open orders, for the given currency pair.
+	 * 
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @return (String) The account open orders for the currency pair.
+	 */
 	public String open_orders(String couple) {
 		return this.apiCall("open_orders", couple, "", true);
 	}
 	
+	/**
+	 * Cancel the account order, with the given ID. 
+	 * 
+	 * @param id (Int) = The order ID number
+	 * @return (String) = The successfulness of the order cancellation:
+	 * (True/False).
+	 */
 	public String cancel_order(int id) {
 		return this.apiCall("cancel_order", "", ("id," + id), true);
 	}
 	
+	/**
+	 * Place an order, via the Cex.io API, for the given currency pair,
+	 * with the given amount and price.
+	 * 
+	 * @param couple (String) = Cex.io currency pair for the POST request.
+	 * @param type (String) = Order type (buy/sell). 
+	 * @param amount (Float) = Order amount.
+	 * @param price (Float) = Order price.
+	 * @return (String) = The order information, including: the order
+	 * id, time, pending, amount, type, and price.
+	 */
 	public String place_order(String couple, String type, float amount, float price) {
 		return this.apiCall("place_order", couple, ("type," + type + ",amount," + amount + ",price," + price), true);
 	}
